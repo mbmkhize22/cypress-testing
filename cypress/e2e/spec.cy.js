ad31e2acc1d65774d53a6042594e9d0505c7c03c
Nhlanhla Mkhize <mbmkhize22@gmail.com>
2023-10-12 15:52:19 +0200

    radio buttons test working

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,5 +3,6 @@ describe('template spec', () => {
     cy.visit('https://google.com')
     cy.get('#APjFqb').type('Zamambo Telecoms {enter}')
     cy.get(':nth-child(2) > .g > .N54PNb > .jGGQ5e > .yuRUbf > :nth-child(1) > [jscontroller="msmzHf"] > a > .LC20lb').click()
+
   })
 })
--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -1,5 +1,38 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://zamambo.co.za')
+describe('Wonder Lab Test Spec', () => {
+  it('Radio Buttons', () => {
+    cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+
+    // Checking Radio button 3
+    cy.get('[for="radio3"]').find('input').check()
+
+    // Validating that only radio button 2 is selected
+    cy.get('#radio-btn-example > fieldset').find('label').each(($label, index) => {
+        if(index == 0)
+            cy.wrap($label).find('input').should('not.be.checked')
+
+        if(index == 1)
+            cy.wrap($label).find('input').should('not.be.checked')
+        
+        if(index == 2)
+            cy.wrap($label).find('input').should('be.checked')
+    })
+
+    // waiting so that you can see that Radio button was selected
+    cy.wait(5000)
+
+    // Checking Radio button 2
+    cy.get('[for="radio2"]').find('input').check()
+
+    // Validating that only radio button 2 is selected
+    cy.get('#radio-btn-example > fieldset').find('label').each(($label, index) => {
+        if(index == 0)
+            cy.wrap($label).find('input').should('not.be.checked')
+
+        if(index == 1)
+            cy.wrap($label).find('input').should('be.checked')
+        
+        if(index == 2)
+            cy.wrap($label).find('input').should('not.be.checked')
+    })
   })
 })
